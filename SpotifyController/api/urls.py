595ce6c863e6e979b68931fa2675d8b046ae532f
Andres Reyna <andresreyna15@gmail.com>
2023-12-07 01:04:09 -0600

    Added first post (Chapter 5 Finished

--- SpotifyController/api/urls.py
+++ SpotifyController/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import main, RoomView
+from .views import main, RoomView, CreateRoomView
 
 urlpatterns = [
     path('home/', main),
-    path('rooms/', RoomView.as_view())
+    path('rooms/', RoomView.as_view()),
+    path('create-room/', CreateRoomView.as_view())
 ]
